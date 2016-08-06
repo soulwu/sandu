a1ab2ad7c8236171681da1e57df63135c9c200f7
吴静波 <wujingbo@noahwm.com>
2016-08-06 16:58:26 +0800

    update修复

---
+++ updates/0.0.3-update.js
@@ -0,0 +1,6 @@
+var keystone = require('keystone');
+var PostCategory = keystone.list('PostCategory');
+
+exports = module.exports = function(done) {
+	PostCategory.model.update({}, {order: 999, isShow: true}, {multi: true}, done);
+};
